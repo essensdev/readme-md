6b1983ed58ec188a836b6bf42da1e635a7c86dc6
0xShapeShifter <113918140+0xShapeShifter@users.noreply.github.com>
2023-02-01 20:13:27 +0300

    update: web3 skills section

--- src/components/inputs/Skills.svelte
+++ src/components/inputs/Skills.svelte
@@ -1,9 +1,9 @@
 <script>
   import BackendSkills from "./BackendSkills.svelte";
-  import Web3Skills from "./web3Skills.svelte";
   import CoreSkills from "./CoreSkills.svelte";
   import FrontendSkills from "./FrontendSkills.svelte";
   import Note from "./Note.svelte";
+  import Web3Skills from "./Web3Skills.svelte";
 </script>
 
 <div class="mb-10 md:mb-0">
